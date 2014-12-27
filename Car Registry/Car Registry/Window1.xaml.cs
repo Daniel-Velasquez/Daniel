c0ec4fbee620d46a2b261c81e239a92311890782
Daniel Amadeo V. Velasquez <danielvelasquez50@yahoo.com>
2014-12-27 00:29:29 -0800

    Source tree commit test

---
+++ Car Registry/Car Registry/Window1.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Car_Registry
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Car myNewCar = new Car();
+
+            myNewCar.Make = Make.Text;
+            myNewCar.Make = Model.Text;
+            myNewCar.Make = Year.Text;
+            myNewCar.Make = Color.Text;
+            myNewCar.Make = Capacity.Text;
+
+
+
+            Result.Text = "\n--------------------------------------------------------------------------------------" + 
+                "\n\t\tYOU REGISTERED THIS CAR" + "\n\nCar Name: " + Make.Text + "\nCar Model: " + Model.Text + "\nCar Year: " + Year.Text
+               + "\nCar Color: " + Color.Text + "\nCar Capacity: " + Capacity.Text ;
+        
+         string carRegistry = Result.Text;
+         System.IO.File.AppendAllText(@"C:\Users\Serbisyong Bayan\Desktop\Daniel Files\Car Registry\Registered Cars.txt", carRegistry.Replace("\n", Environment.NewLine));
+        
+
+            
+
+
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+          
+            
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Color { get; set; }
+        public string Capacity { get; set; }
+    }
+
+}
